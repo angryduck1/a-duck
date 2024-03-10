42c61e60a4f174fa4165328e068d37a944356553
angryduck <98224800+angryduck1@users.noreply.github.com>
2024-03-10 13:53:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function button1(){
+    document.getElementById("mom1") .innerHTML = "Самая лучшая";
+}
+function button2(){
+    document.getElementById("mom2") .innerHTML = "Самая прекрасная";
+}
+function button3(){
+    document.getElementById("mom3") .innerHTML = "Самая очеровательная";
+}
