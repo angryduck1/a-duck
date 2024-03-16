35096c29d9bca6939b96c4b371b6a1f41b49a971
angryduck <98224800+angryduck1@users.noreply.github.com>
2024-03-16 10:19:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function button1(){
-    document.getElementById("mom1") .innerHTML = "Самая лучшая";
+function button(){
+    const move = " 56% "
+    document.getElementById("move1") .innerHTML = "Макдональдс выбирают" + move + "людей";
 }
 function button2(){
-    document.getElementById("mom2") .innerHTML = "Самая прекрасная";
-}
-function button3(){
-    document.getElementById("mom3") .innerHTML = "Самая очеровательная";
+    const move = " 44% "
+    document.getElementById("move2") .innerHTML = "КФС выбирают" + move + "людей";
 }
