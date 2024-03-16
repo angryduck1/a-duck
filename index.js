3efafa3f5085640f542339c7ffc8a4062d7be9f0
angryduck <98224800+angryduck1@users.noreply.github.com>
2024-03-16 10:28:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function button(){
-    const move = " 56% "
-    document.getElementById("move1") .innerHTML = "Макдональдс выбирают" + move + "людей";
+    document.getElementById("move1") .innerHTML = "Макдональдс выбирают 56% людей";
 }
 function button2(){
-    const move = " 44% "
-    document.getElementById("move2") .innerHTML = "КФС выбирают" + move + "людей";
+    document.getElementById("move2") .innerHTML = "КФС выбирают 44% людей";
 }
